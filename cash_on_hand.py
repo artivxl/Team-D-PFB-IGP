38f36c0d4aef06920fbd7f710166c5b7a657a666
rachelchia <s10256155@connect.np.edu.sg>
2023-08-01 21:38:31 +0800

    cash_on_hand

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import csv
 
 # creating a file to csv file
-fp = Path.cwd() / "Cash_on_hand.csv"
+fp = Path.cwd() / "Cash_on_Hand.csv"
 
 # read the csv file to append profit and quantity from the csv
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
@@ -37,20 +37,25 @@ def calculate_CashOnHand_difference(cash_on_hand):
         # check if the difference of the cash on hand is increasing/ decreasing 
         if current_cash_surplus < previous_cash_surplus:
             cash_deficits.append((cash_on_hand[value][0], difference))
-        else:
-            highest_cash_surplus = max(highest_cash_surplus, difference)
+        
+        # if the surplus of the current is cash on hand is higher than the previous
+        # current surplus will be the new "highest cash surplus"
+        # day of the new "highest cash surplus" will be recorded as well
+        if difference > highest_cash_surplus:
+            highest_cash_surplus = difference
+            highest_cash_surplus_day = cash_on_hand[value][0]
 
-    # store highest cash surplus and any cash deficits into separate variables
-    return highest_cash_surplus, cash_deficits
+    # store highest cash surplus, its day and any other cash deficits into separate variables
+    return highest_cash_surplus, highest_cash_surplus_day, cash_deficits
 
 
-def print_cash_surplus(highest_cash_surplus):
+def print_cash_surplus(highest_cash_surplus, highest_cash_surplus_day):
     """
     - This function formats and prints the results according to the format in the print statement 
     - Parameter required: highest_cash_surplus
     """
     print("[CASH SURPLUS]: CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
-    print(f"[HIGHEST CASH SURPLUS]: AMOUNT: USD{highest_cash_surplus}")
+    print(f"[HIGHEST CASH SURPLUS]: DAY: {highest_cash_surplus_day}, AMOUNT: USD{highest_cash_surplus}")
 
 
 def print_cash_deficit(cash_deficits):
@@ -63,11 +68,11 @@ def print_cash_deficit(cash_deficits):
         # abs is used to ensure that all the values are positive
 
 # calls fuctions and assigns return values to 2 separate variables 
-highest_cash_surplus, cash_deficits = calculate_CashOnHand_difference(cash_on_hand)
+highest_cash_surplus, highest_cash_surplus_day, cash_deficits = calculate_CashOnHand_difference(cash_on_hand)
 
 # if there are cash deficits, then it will only print the deficts and the day
 if cash_deficits:
     print_cash_deficit(cash_deficits)
 # if there are no cash deficits where it keeps profitting, it will only print cash surplus and the day
 elif highest_cash_surplus > 0:
-    print_cash_surplus(highest_cash_surplus)
+    print_cash_surplus(highest_cash_surplus, highest_cash_surplus_day)
