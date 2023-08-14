f595826cff2d3e797bd7e1ea3617759b549eaede
tingggggggggggg <S10257717@connect.np.edu.sg>
2023-08-14 11:01:31 +0800

    profit loss

--- profit_loss.py
+++ profit_loss.py
@@ -4,25 +4,25 @@ import csv
 # create a file to csv file.
 fp = Path.cwd()/"csv_report"/"Profits_and_loss.csv"
 
-# read the csv file to append day and net profit from the csv.
+# read the csv file to append day and net profit from the csv
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) # skip header
 
-    # create an empty lists to store day and net profit
+    # create an empty list to store day and net profit
     profitLoss=[] 
 
     # append day and net profit into the profitLoss list
     for row in reader:
-        #get the day and net profit each day record
-        #and append the profitLoss list
+        # get the day and net profit each day record
+        # and append the profitLoss list
         profitLoss.append([row[0],int(row[4])])
 
 
 def calculate_netProfit_difference(profitLoss):
     """
-    - This function will calculate the difference in net profit and store the highest profit increase and its corresponding day. 
-    - As well as the list of profit loss days and the differences.
+    - This function will calculate the difference in net profit and store the highest profit increase and its corresponding day
+    - as well as the list of profit loss days and the differences
     - Parameter required: profitLoss
     """
     highest_profit_increase = 0
